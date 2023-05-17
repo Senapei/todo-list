949df5bbfa91dddaeee866e09908457fbdbded87
Senapei <115939642+Senapei@users.noreply.github.com>
2023-05-17 12:43:10 +0100

    Add todays date as title and add items to the list

--- app.js
+++ app.js
@@ -4,13 +4,26 @@ import * as url from "url";
 
 const app = express();
 const port = 3000;
+app.use(express.urlencoded({ extended: true }));
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 app.set("view engine", "ejs");
 
+let listArray = [];
+
 app.get("/", (req, res) => {
-  res.render("list");
+  let date = new Date();
+  let options = { weekday: "long", day: "numeric" };
+
+  let dayTitle = date.toLocaleDateString("GB", options);
+  res.render("list", { date: dayTitle, items: listArray });
+});
+
+app.post("/", (req, res) => {
+  let newItem = req.body.item;
+  listArray.push(newItem);
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
